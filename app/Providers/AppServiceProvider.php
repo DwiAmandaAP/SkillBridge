e2276c805642ea210e25165a8bacf0dcaf4e0a80
Dwi Amanda <Dwiamandaameliaputri@gmail.com>
2026-09-19 22:46:44 +0700

    dokumentasi api

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Services\Scraping\JobSourceInterface;
 use App\Services\Scraping\KarirhubJobSource;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
     }
 }
